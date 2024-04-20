6a34499752c8406738da4f23797b07091d0ee9a7
Slavahaim <slava.irizanov@gmail.com>
2024-04-20 20:56:01 +0300

    some fixes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from "express";
-
 import dotenv from "dotenv";
 import bodyParser from 'body-parser';
 
@@ -13,17 +12,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', async (req: Request, res: Response) => {
   res.status(200).json({
-    message: 'Hello World'
+    message: 'Hello World!'
   });
 });
 
-// app.get('/', (req: Request, res: Response) => {
-//     res.status(200).send("Hello, World!!!!");
-// })
 
-app.listen(PORT, () => { 
-    console.log("Server running at PORT: ", PORT); 
-  }).on("error", (error) => {
-    // gracefully handle error
-    throw new Error(error.message);
-  })
+const start = async () => {
+  try {
+      app.listen(PORT, () => console.log(`Server started on port - ${PORT}`))
+  } catch (error){
+      console.error(error)
+  }
+}
+
+start()
--- src/model/email.ts
+++ src/model/email.ts
@@ -4,5 +4,5 @@ export interface Email {
     to: string;
     subject: string;
     body: string;
-  }
+}
   
